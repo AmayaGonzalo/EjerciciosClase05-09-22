e96dc154c952f8e2314b45ee1048201fdaae39ec
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-09-12 01:24:23 -0300

    primer Comit

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("hola mundo");
+let readlineSync = require('readline-sync');
+let alturaPersona = readlineSync.question("Indique la altura de la persona: ");
+console.log("su altura es: ",alturaPersona);
+
+
+let mensaje = readlineSync.question("Escriba su mensaje: ");
+console.log("Su mensaje escrito es: ",mensaje);
+
+let primerMensaje = readlineSync.question("Escriba su primer mensaje: ");
+let segundoMensaje = readlineSync.question("Escriba su segundo mensaje: ");
+let tercerMensaje = readlineSync.question("Escriba su tercer mensaje: ");
+console.log("Su primer mensaje: ",primerMensaje,". Su segundo mensaje: ",segundoMensaje,". Su tercer mensaje: ",tercerMensaje,".");
+
+
+function calcularArea(){
+let base = parseInt (readlineSync.question("Ingrese la base: "));
+let altura = parseInt (readlineSync.question("Ingrese la altura: "));
+resultado = base * altura;
+return resultado
+};
+calcularArea();
+ console.log("El area del cuadrado es: ",resultado);
+
+
+let primeraVuelta = readlineSync.questionInt("Ingrese el tiempo de la primera vuelta: ");
+let segundaVuelta = readlineSync.questionInt("Ingrese el tiempo de la segunda vuelta: ");
+let terceraVuelta = readlineSync.questionInt("Ingrese el tiempo de la tercera vuelta: ");
+let cuartaVuelta = readlineSync.questionInt("Ingrese el tiempo de la cuarta vuelta: ");
+let totalVuelta = primeraVuelta + segundaVuelta + terceraVuelta + cuartaVuelta;
+let promedioVuelta = totalVuelta / 4;
+console.log("El total de tiempo de vueltas es: ",totalVuelta);
+console.log("El promedio de tiempo de vueltas es: ",promedioVuelta);
