30b540e70a7e959a5905a05d52e23e45a7c70b39
irinamarzioni <imarzioni@devartis.com>
2022-09-13 09:09:30 -0300

    correccion

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-console.log("hola mundo");
+let holaMundo = "hola mundo";
+console.log(holaMundo);
+
 let readlineSync = require('readline-sync');
 let alturaPersona = readlineSync.question("Indique la altura de la persona: ");
-console.log("su altura es: ",alturaPersona);
+console.log("su altura es: ", alturaPersona);
 
 
 let mensaje = readlineSync.question("Escriba su mensaje: ");
-console.log("Su mensaje escrito es: ",mensaje);
+console.log("Su mensaje escrito es: ", mensaje);
 
 let primerMensaje = readlineSync.question("Escriba su primer mensaje: ");
 let segundoMensaje = readlineSync.question("Escriba su segundo mensaje: ");
 let tercerMensaje = readlineSync.question("Escriba su tercer mensaje: ");
-console.log("Su primer mensaje: ",primerMensaje,". Su segundo mensaje: ",segundoMensaje,". Su tercer mensaje: ",tercerMensaje,".");
+console.log("Su primer mensaje: ", primerMensaje, ". Su segundo mensaje: ", segundoMensaje, ". Su tercer mensaje: ", tercerMensaje, ".");
 
 
 function calcularArea(){
-let base = parseInt (readlineSync.question("Ingrese la base: "));
-let altura = parseInt (readlineSync.question("Ingrese la altura: "));
-resultado = base * altura;
-return resultado
+  let base = parseInt (readlineSync.question("Ingrese la base: "));
+  let altura = parseInt (readlineSync.question("Ingrese la altura: "));
+  return base * altura;
 };
-calcularArea();
- console.log("El area del cuadrado es: ",resultado);
+let resultado = calcularArea();
+console.log("El area del cuadrado es: ", resultado);
 
 
 let primeraVuelta = readlineSync.questionInt("Ingrese el tiempo de la primera vuelta: ");
@@ -29,5 +30,5 @@ let terceraVuelta = readlineSync.questionInt("Ingrese el tiempo de la tercera vu
 let cuartaVuelta = readlineSync.questionInt("Ingrese el tiempo de la cuarta vuelta: ");
 let totalVuelta = primeraVuelta + segundaVuelta + terceraVuelta + cuartaVuelta;
 let promedioVuelta = totalVuelta / 4;
-console.log("El total de tiempo de vueltas es: ",totalVuelta);
-console.log("El promedio de tiempo de vueltas es: ",promedioVuelta);
+console.log("El total de tiempo de vueltas es: ", totalVuelta);
+console.log("El promedio de tiempo de vueltas es: ", promedioVuelta);
